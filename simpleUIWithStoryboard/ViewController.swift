7c48b988d4a965ce274fec4614f23ac64aaf334f
Sunil Dutt <sunil@shakunsun.com>
2017-09-30 14:20:54 +0530

    Initial Commit

---
+++ simpleUIWithStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleUIWithStoryboard
+//
+//  Created by Sunil Dutt on 30/09/17.
+//  Copyright © 2017 Sunil Dutt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
